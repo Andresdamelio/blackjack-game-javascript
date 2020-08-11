e35c798e597caf6719ac6649c117a8af609fe18d
Andres D' Amelio <andresdamelio24@gmail.com>
2020-08-10 22:39:27 -0400

    Función para tomar una carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -8,26 +8,39 @@
 
 */
 
-let deck = [];
+let decks = [];
 const types = ["C", "D", "H", "S"];
 const specials = ["A", "J", "Q", "K"];
 
+// Función para crear una nueva baraja
 const createDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (const type of types) {
-      deck.push(i + type);
+      decks.push(i + type);
     }
   }
 
   for (const type of types) {
     for (const special of specials) {
-      deck.push(special + type);
+      decks.push(special + type);
     }
   }
 
-  console.log(deck);
-  deck = _.shuffle(deck);
-  console.log(deck);
+  decks = _.shuffle(decks);
 };
 
 createDeck();
+
+const takeADeck = () => {
+  if (!decks.length) {
+    throw new Error("No hay más cartas");
+  }
+
+  const deck = decks.pop();
+
+  return deck;
+};
+
+decks = [];
+
+takeADeck();
